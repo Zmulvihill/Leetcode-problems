2f0e821c08af0ad02df0c5ea06274fad9f8db8df
Zmulvihill <zmulvihi@yahoo.com>
2023-04-10 19:26:15 -0700

    added longestPrefix from leetcode and added pseudocode, not solved yet

---
+++ longestPrefix.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    //write function to find the longest common prefix string amongst an array of strings//
+    //if no common prefix return an empty string//
+    //if first letter matches move to next, then the next//
+    //until it no longer matches, then return longest string//
+    //if no string matches return ""//  
+  };
