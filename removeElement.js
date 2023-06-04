a9a37e04593a48165a545c4a07c89a14a5845a52
Zmulvihill <zmulvihi@yahoo.com>
2023-06-03 17:02:29 -0700

    worked on removeElement and saved progress

--- removeElement.js
+++ removeElement.js
@@ -1,8 +1,16 @@
 //https://leetcode.com/problems/remove-element/
 
 class Solution {
-    public:
-        int removeElement(vector<int>& nums, int val) {
-            
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
         }
-    };
+        return i;
+    }
+}
