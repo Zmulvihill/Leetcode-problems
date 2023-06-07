bd9199c857857ba217076674501213b1a0531ef7
Zmulvihill <zmulvihi@yahoo.com>
2023-06-06 19:08:11 -0700

    finished removeElement and saved, passed

--- removeElement.js
+++ removeElement.js
@@ -2,15 +2,15 @@
 
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int i = 0;
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != val) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                i++;
+        if (nums.length == 0) return 0;
+
+        int valid_size = 0;
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] != val) {
+                nums[valid_size] = nums[i];
+                valid_size++;
             }
         }
-        return i;
+        return valid_size;
     }
 }
