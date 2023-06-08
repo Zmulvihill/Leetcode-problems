c8c84732dd6b27a94b825a7d5fdeda8c17676fa5
Zmulvihill <zmulvihi@yahoo.com>
2023-06-07 17:24:26 -0700

    worked on mergedLists, didn't finish or pass, still needs work

--- mergedLists.js
+++ mergedLists.js
@@ -1,1 +1,30 @@
 //https://leetcode.com/problems/merge-two-sorted-lists/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+var mergeTwoLists = function(list1, list2) {
+    let tempNode = new ListNode(0, null);
+    let currentNode = tempNode;
+    
+    while (l1 && l2) {
+        if(l1.val < l2.val) {
+            currentNode.next = l1;
+            l1 = l1.next
+        } else {
+            currentNode.next = l2;
+            l2 = l2.next
+        }
+        currentNode = currentNode.next;
+    }
+    currentNode.next = l1 || l2;
+    
+    return tempNode.next;
+};
