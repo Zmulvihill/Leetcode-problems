09a35bd204589118c205aee760fa024211ec3167
Zmulvihill <zmulvihi@yahoo.com>
2023-06-08 13:44:54 -0700

    worked on mergedLists, finished, passed, and saved

--- mergedLists.js
+++ mergedLists.js
@@ -10,21 +10,24 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-var mergeTwoLists = function(list1, list2) {
-    let tempNode = new ListNode(0, null);
-    let currentNode = tempNode;
-    
-    while (l1 && l2) {
-        if(l1.val < l2.val) {
-            currentNode.next = l1;
-            l1 = l1.next
+var mergeTwoLists = function(l1, l2) {
+    const dummy = new ListNode(-Infinity);
+    let prev = dummy;
+
+    while(l1 && l2) {
+        if(l1.val <= l2.val) {
+            prev.next = l1;
+            prev = l1;
+            l1 = l1.next;
         } else {
-            currentNode.next = l2;
-            l2 = l2.next
+            prev.next = l2;
+            prev = l2;
+            l2 = l2.next;
         }
-        currentNode = currentNode.next;
     }
-    currentNode.next = l1 || l2;
-    
-    return tempNode.next;
+
+    if(!l1) prev.next = l2;
+    if(!l2) prev.next = l1;
+
+    return dummy.next;
 };
