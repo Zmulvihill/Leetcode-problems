b3175037b04be773fbdbb049d598c10bb89362a7
Zmulvihill <zmulvihi@yahoo.com>
2023-06-12 20:25:23 -0700

    added pseudocode to LengthOflastWord, and saved

--- lengthOfLastWord.js
+++ lengthOfLastWord.js
@@ -6,4 +6,14 @@
  */
 var lengthOfLastWord = function(s) {
     
+    /*
+    1. Create lastWordCount variable
+    2. Loop backwards s.
+        a. condition if current char is not a space
+            i. loop backwards for current char
+                1. Condition if current char is not a space
+                    a. increment lastWordCount
+                2. Else return lastWordCount
+            ii. return lastWordCount
+    */
 };
