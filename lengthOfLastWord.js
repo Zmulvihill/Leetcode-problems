1fe0b082269ac4e15ee05a527266d7e13b61b637
Zmulvihill <zmulvihi@yahoo.com>
2023-06-14 18:28:23 -0700

    finished lengthOfLastWord and ran, passed, and saved

--- lengthOfLastWord.js
+++ lengthOfLastWord.js
@@ -8,9 +8,17 @@ var lengthOfLastWord = function(s) {
     let lastWordCount = 0;
     for (let i = s.length -1; i >=0; i--) {
         if (s[i] !== " "){
-            
+            for (let j = i; j >= 0; j--){
+                if(s[j] !== " "){
+                    lastWordCount++;
+                } else {
+                    return lastWordCount;
+                }
+            }
+            return lastWordCount;
         }
     }
+};
 
     /*
     1. Create lastWordCount variable
