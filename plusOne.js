136a353e50ba09eee6d62caf5ddf61a8460df9d1
Zmulvihill <zmulvihi@yahoo.com>
2023-06-23 19:14:01 -0700

    added psuedocode to plusOne and saved

---
+++ plusOne.js
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/plus-one/
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+    
+    /*
+    1. Loop digits backwards
+        a. If value is less than 9
+            i. Increment value
+            ii. Return digits
+        b. Change current value to 0
+    2. Add 1 to the front of the digits
+    3. Return digits
+    */
+};
