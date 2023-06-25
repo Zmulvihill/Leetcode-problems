f1e03f1b1f3a280567a366c503dd44182bb162ae
Zmulvihill <zmulvihi@yahoo.com>
2023-06-24 20:04:52 -0700

    added code and saved

--- plusOne.js
+++ plusOne.js
@@ -14,4 +14,13 @@ var plusOne = function(digits) {
     2. Add 1 to the front of the digits
     3. Return digits
     */
+   for (let i = digits.length -1; i >= 0; i--){
+    if (digits[i] < 9){
+        digits[i]++;
+        return digits;
+    }
+    digits[i] = 0;
+   }
+   digits.unshift(1);
+   return digits;
 };
